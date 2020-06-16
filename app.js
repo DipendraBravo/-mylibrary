ce866667c3c2971827d1dc10dd94e2c53e5b4aca
Dipendra Tamang <50724359+DipendraBravo@users.noreply.github.com>
2020-06-16 14:13:09 +0545

    initial route

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+    require('dotenv').config({ path: '.env' })
 }
 
 const express = require('express')
@@ -7,16 +7,18 @@ const app = express()
 const expresslayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 const indexRouter = require('./routes/index')
-    //connection
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-})
+const authorRouter = require('./routes/author')
+const bodyparser = require('body-parser');
+
+
+//connection
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log("connected to Mongoose"))
 
 
-
+app.use(bodyparser.urlencoded({ limit: "10mb", extended: false }))
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
@@ -24,5 +26,6 @@ app.use(expresslayouts)
 app.use(express.static('public'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
